ddb5002dd08c722e1bd4365d89cc37cb9b761a25
aaron_mordeno <62050481+aaron-mordeno@users.noreply.github.com>
2023-04-06 11:11:02 +0800

    Added light/dark theme switch functionality

--- js/script.js
+++ js/script.js
@@ -30,11 +30,48 @@
     e("body").scrollspy({ target: "#sideNav" });
 })(jQuery);
 
+// light and dark theme toggle
+const toggleSwitch = document.getElementById("toggle-switch");
+toggleSwitch.addEventListener("click", switchTheme);
+
+function switchTheme() {
+  if (toggleSwitch.classList.contains("light-theme")) {
+    toggleSwitch.classList.remove("light-theme");
+    toggleSwitch.classList.add("dark-theme");
+    darkTheme();
+  } else if (toggleSwitch.classList.contains("dark-theme")) {
+    toggleSwitch.classList.remove("dark-theme");
+    toggleSwitch.classList.add("light-theme");
+    lightTheme();
+  }
+}
+
+function darkTheme() {
+  document.documentElement.style.setProperty("--background-color", "#343a40");
+  document.documentElement.style.setProperty("--body-color", "#fff");
+  document.documentElement.style.setProperty("--header-color", "#fff");
+  document.documentElement.style.setProperty("--card-header", "#fff");
+  document.documentElement.style.setProperty("--card-text", "#fff");
+  document.documentElement.style.setProperty("--card-border", "#fff");
+  document.documentElement.style.setProperty("--card-background", "#343a40");
+}
+
+function lightTheme() {
+  document.documentElement.style.setProperty("--background-color", "#fff");
+  document.documentElement.style.setProperty("--body-color", "#343a40");
+  document.documentElement.style.setProperty("--header-color", "#343a40");
+  document.documentElement.style.setProperty("--card-header", "#343a40");
+  document.documentElement.style.setProperty("--card-text", "#868e96");
+  document.documentElement.style.setProperty("--card-border", "#868e96");
+  document.documentElement.style.setProperty("--card-background", "#fff");
+}
+
+// form validation
 const form = document.getElementsByTagName("form");
 const name = document.getElementById("inputName");
 const email = document.getElementById("inputEmail");
-const emailFormat =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+// const emailFormat =
+//   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const subject = document.getElementById("inputSubject");
 const message = document.getElementById("inputMessage");
 const send = document.getElementById("send");
